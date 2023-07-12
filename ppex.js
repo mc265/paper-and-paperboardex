48b995c30cf7db22a4e4167ec2a1faad50d5049b
mchafulumira <125554742+mc265@users.noreply.github.com>
2023-07-12 09:28:15 +0200

    Update ppex.js

--- ppex.js
+++ ppex.js
@@ -27,7 +27,7 @@ enabled:false
 },
 
         title: {
-            text: 'Paper and paperboard export'
+            text: 'Export of key wood products over time. Press the name of any wood product at bottom to see it displayed.  Grab the edges of the bottom bar to shrink and expand the years displayed in the main graph'
         },
     
            xAxis: {
